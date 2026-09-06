a2209cc64d3ba7970087b3a2821c77424a00f661
Tamás Gulácsi <tamas@gulacsi.eu>
2026-09-06 15:01:13 +0200

    EUPL-1.2

--- mnb/types.go
+++ mnb/types.go
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 //
-// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: EUPL-1.2
 
 package mnb
 
